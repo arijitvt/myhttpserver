d0ba3dd1f27e3cda14ff495626dd60089adf69ab
Arijit Chattopadhyay <arijitnit06@gmail.com>
2022-07-14 17:07:11 -0400

    data

--- main.go
+++ main.go
@@ -2,29 +2,27 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-func scheduleAlarm(response http.ResponseWriter, req *http.Request) {
-	fmt.Println("Server")
-	output := "Hello From the server"
-	fmt.Fprintf(response, "Hello %q ", output)
+type Hello {
+	Name string
+}
+
+func scheduleAlarm(c *gin.Context) {
+	fmt.Println("Getting request")
+	obj := Hello()
+	obj.Name = "arijit"
+	c.JSON(200,obj)
 }
 
 func main() {
 	router := gin.Default()
 	port := os.Getenv("PORT")
 	fmt.Println("Starting scheduling server -----> ", port)
-	httpServer := &http.Server{
-		Addr:           ":" + port,
-		Handler:        router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-	httpServer.ListenAndServe()
+
+	router.GET("/alarm", scheduleAlarm)
+	router.Run(":" + port)
 }
