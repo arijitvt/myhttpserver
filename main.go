52afcc1494d782ea47e025e473d153f205b146fb
Arijit Chattopadhyay <arijitnit06@gmail.com>
2022-07-14 15:20:09 -0400

    first server

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
-import print 
+import (
+	"fmt"
+	"net/http"
+
+)
+
+func scheduleAlarm(response http.ResponseWriter, req *http.Request) {
+	fmt.Println("Server")
+}
 
 func main() {
-	fmt.Println("Arijit")
+	fmt.Println("Starting scheduling server ----->")
+	http.HandleFunc("/alarm", scheduleAlarm)
+	http.ListenAndServer(":10000",nil)
 }
---
+++ scheduler/scheduler.go
@@ -0,0 +1,9 @@
+package "scheduler"
+
+type Job struct {
+	JobName  String
+}
+
+func Schedule(job Job) {
+
+}
