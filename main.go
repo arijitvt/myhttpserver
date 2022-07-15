5878e2a6ad5584b703a1c6699bb5af93691c8682
Arijit Chattopadhyay <arijitnit06@gmail.com>
2022-07-14 21:00:54 -0400

    pushing

--- main.go
+++ main.go
@@ -2,27 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Hello struct {
-	Name string
+type Response struct {
+	Message string
 }
 
-func scheduleAlarm(c *gin.Context) {
-	fmt.Println("Getting request")
-	var obj Hello
-	obj.Name = "arijit"
-	c.JSON(200, obj)
+func AddJob(c *gin.Context) {
+	var response Response
+	response.Message = "SUCCESS"
+	c.JSON(http.StatusOK, response)
 }
 
 func main() {
 	router := gin.Default()
 	port := os.Getenv("PORT")
 	fmt.Println("Starting scheduling server -----> ", port)
-
-	router.GET("/alarm", scheduleAlarm)
+	router.LoadHTMLGlob("templates/alarm.tmpl")
+	router.GET("/alarm", AddJob)
 	router.Run(":" + port)
 }
--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,11 +1,28 @@
 package scheduler
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type JobData struct {
 	JobId string
 }
 
-func Schedule(job JobData) {
+type Scheduler struct {
+	Id string
+}
+
+func AddJob(c *gin.Context, scheduler *Scheduler, job JobData) {
+	c.HTML(http.StatusOK, "alarm.tmpl", gin.H{})
 
 }
 
+func RemoveJobStr(c *gin.Context, sch *Scheduler, jobId string) {
 
+}
+
+func RemoveJob(c *gin.Context, scheduler *Scheduler, job JobData) {
+
+}
